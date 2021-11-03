381021ceaf11009554007667627ad87b20d840a5
Alexander Zakharchuk <mystang.pro@gmail.com>
2021-11-03 17:43:55 +0300

    Added files for heroku

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
+from secrets import token_urlsafe
 
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "a;sdgoharpgihaeprih"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///../db.db"
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", token_urlsafe(32))
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+    "DATABASE_URL", "sqlite:///../db.db")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
